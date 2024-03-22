91ea71477a536d9849d870c6c9705e7dfb65c99d
Vinícius José <157813846+Vyneelric@users.noreply.github.com>
2024-03-22 16:13:50 -0300

    Add files via upload

--- funções/exemplo01.py
+++ funções/exemplo01.py
@@ -9,6 +9,8 @@ def mostrar_opcoes():
     print('2 - Subtração')
     print('3 - Multiplicação')
     print('4 - Divisão')
+    print('5 - Somátoria')
+    print('6 - Fatorial')
     print('0 - Sair')
 
 #função para pegar os valores para o resultado
@@ -28,7 +30,24 @@ def mult(a,b):
     return a * b
 
 def div(a,b):
-    return a / b
+    return a/b
+
+def somatorio(numero):
+    contador = 1
+    s = 0
+    while contador <= numero:
+        s = s + contador 
+        contador = contador + 1
+    return s
+    
+def fato(numero):
+    contador = 1
+    f = 1
+    while contador <= numero:
+        f = f * contador 
+        contador = contador + 1
+    return f
+
 
 #repetição para funcionar o código e fazer a repetição
 resposta = 1
@@ -51,3 +70,19 @@ while resposta != '0':
         n1, n2 = pegar_valores()
         resultado = div(n1,n2)
         print(f'A divisão entre os números foi de: {resultado}')
+    elif resposta == '5':
+        n1, n2 = pegar_valores()
+        resultado1 = somatorio(n1)
+        resultado2 = somatorio(n2)
+        print(f'A somatória de {n1} é: {resultado1}')
+        print(f'A somatória de {n2} é: {resultado2}')
+    elif resposta == '6':
+        n1, n2 = pegar_valores()
+        resultado3 = fato(n1)
+        resultado4 = fato(n2)
+        print(f'O fatorial de {n1} é: {resultado3}')
+        print(f'O fatorial de {n2} é: {resultado4}')
+    elif resposta == '0':
+        print ('saindo do system...')
+    else:
+        print ('Opção inválida, tente novamente')
